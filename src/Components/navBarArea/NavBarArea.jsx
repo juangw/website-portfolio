284c0cb3cda5cf534ee599adc07bad43ec2a2b1d
William Juang <juangw@umich.edu>
2019-12-13 22:30:04 -0500

    resume setup

--- src/Components/navBarArea/NavBarArea.jsx
+++ src/Components/navBarArea/NavBarArea.jsx
@@ -10,7 +10,8 @@ class NavBarArea extends Component {
                         <Link to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/resume">Resume</Link>
+                        <a href={'/Resume-WilliamJuang.pdf'}>Resume</a>
+                        {/* <Link to="/Resume-WilliamJuang.pdf">Resume</Link> */}
                     </li>
                     <li>
                         <Link to="/projects">Projects</Link>
